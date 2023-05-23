0654166bec219fc27462c3629358e6d7f84bf769
Alvinouille <enjoy011113@gmail.com>
2023-05-23 17:56:41 +0200

    DONE

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -1,20 +1,33 @@
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
+#include <iomanip>
 
-Account::Account(int initial_deposit) : _amount(initial_deposit), nbDeposits(0), _nbWithdrawals(0), _accountIndex(0)
+int Account::_nbAccounts(0);
+int Account::_totalAmount(0);
+int Account::_totalNbDeposits(0);
+int Account::_totalNbWithdrawals(0);
+
+Account::Account(int initial_deposit) : _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
 {
     static int i;
 
-    std::cout << "[date] index:" << i << ";amount:" << _amount << ";created" << std::endl;
+    _displayTimestamp();
+    std::cout << "index:" << i << ";amount:" << _amount << ";created" << std::endl;
     _accountIndex = i;
     _nbAccounts++;
+    _totalAmount += initial_deposit;
     i++;
 }
 
 void	Account::makeDeposit(int deposit)
 {
-    std::cout << "[date] index:" << _accountIndex << ";p_amount:" << _amount;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
     _amount += deposit;
+    _totalAmount += deposit;
     _nbDeposits++;
+    _totalNbDeposits++;
     std::cout << ";deposit:" << deposit << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
 }
 
@@ -22,56 +35,78 @@ bool	Account::makeWithdrawal(int withdrawal)
 {
     bool accepted(0);
     
-    std::cout << "[date] index:" << _accountIndex << ";p_amount:" << _amount;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
     if (_amount - withdrawal > 0)
     {
         _amount -= withdrawal;
         accepted = 1;
+        _totalAmount -= withdrawal;
         _nbWithdrawals++;
+        _totalNbWithdrawals++;
     }
     if (accepted)
         std::cout << ";withdrawal:" << withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
     else
         std::cout << ";withdrawal:refused" << std::endl;
+    return (accepted);
 }
 
 Account::~Account(void)
 {
-    std::cout << "[date] index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
 }
 
-static int	Account::getNbAccounts(void)
+int	Account::getNbAccounts(void)
 {
     return (_nbAccounts);
 }
-static int	Account::getTotalAmount(void)
+int	Account::getTotalAmount(void)
 {
     return (_totalAmount);
 }
 
-static int	Account::getNbDeposits(void)
+int	Account::getNbDeposits(void)
 {
     return (_totalNbDeposits);
 }
 
-static int	Account::getNbWithdrawals(void)
+int		Account::checkAmount(void) const
+{
+    return (1);
+}
+
+int	Account::getNbWithdrawals(void)
 {
     return (_totalNbWithdrawals);
 }
 
-static void	Account::displayAccountsInfos(void)
+void	Account::displayAccountsInfos(void)
 {
-    std::cout << "[date] account:" << getNbAccounts() << ";total:" << getTotalAmount()
+    _displayTimestamp();
+    std::cout << "account:" << getNbAccounts() << ";total:" << getTotalAmount()
                 << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
 void    Account::displayStatus(void)const
 {
-    std::cout << "[date] index:" << _accountIndex << ";amount:" << _amount << ";deposit:" << _nbDeposits 
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposit:" << _nbDeposits 
             << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
 
-int Account::_nbAccounts(0);
-int Account::_totalAmount(0);
-int Account::_totalNbDeposits(0);
-int Account::_totalNbWithdrawals(0);
+void	Account::_displayTimestamp( void )
+{
+    int year, month, day;
+    std::time_t current = std::time(NULL);
+    std::tm* now = std::localtime(&current);
+    year = now->tm_year + 1900;
+    month = now->tm_mon + 1;
+    day = now->tm_mday;
+    std::cout << "[" << year << std::setw(2) << std::setfill('0') << month;
+    std::cout << std::setw(2) << std::setfill('0') << day << "_" ;
+    std::cout << std::setw(2) << std::setfill('0') << now->tm_hour;
+    std::cout << std::setw(2) << std::setfill('0') << now->tm_min;
+    std::cout << std::setw(2) << std::setfill('0') << now->tm_sec << "] ";
+}
--- EXEMPLE/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Contact.hpp"
-
-int main(int ac, char **av)
-{
-    (void)ac;
-    Contact nbOne;
-    nbOne.print(av[1]);
-    return (0);
-}
